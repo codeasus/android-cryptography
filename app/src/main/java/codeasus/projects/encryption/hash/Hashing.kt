4bf0574e7f4ea940838a47c657e3ee48850524f6
Orhan Rosenfeld <codeasus.orhan@outlook.com>
2022-12-26 18:53:22 +0400

    Add hashing part

--- app/src/main/java/codeasus/projects/encryption/hash/Hashing.kt
+++ app/src/main/java/codeasus/projects/encryption/hash/Hashing.kt
@@ -1,4 +1,19 @@
 package codeasus.projects.encryption.hash
 
+import java.math.BigInteger
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 class Hashing {
+
+    inner class MD5Hash {
+
+        @kotlin.jvm.Throws(NoSuchAlgorithmException::class)
+        fun generateHash(data: String) {
+            val mD = MessageDigest.getInstance("MD5")
+            val digestRes = mD.digest(data.toByteArray(StandardCharsets.UTF_8))
+
+        }
+    }
 }
