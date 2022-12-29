47b8cb52ac98359e17dc5a155d0c2233d07d7b32
Orhan Rosenfeld <codeasus.orhan@outlook.com>
2022-12-29 16:01:19 +0400

    Commmit last changes

--- app/src/main/java/codeasus/projects/encryption/hash/Hashing.kt
+++ app/src/main/java/codeasus/projects/encryption/hash/Hashing.kt
@@ -1,19 +1,17 @@
 package codeasus.projects.encryption.hash
 
-import java.math.BigInteger
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
-class Hashing {
+object Hashing {
 
-    inner class MD5Hash {
+    object MD5Hash {
 
         @kotlin.jvm.Throws(NoSuchAlgorithmException::class)
         fun generateHash(data: String) {
             val mD = MessageDigest.getInstance("MD5")
             val digestRes = mD.digest(data.toByteArray(StandardCharsets.UTF_8))
-
         }
     }
 }
---
+++ app/src/main/java/codeasus/projects/encryption/hash/HashingPlayground.kt
@@ -0,0 +1,32 @@
+package codeasus.projects.encryption.hash
+
+object CONSTANTS {
+    const val INT_BINARY_SIZE = 32
+}
+
+class NegativeNumberException : Exception()
+
+fun main() {
+    // 1   as 8bit :  0000 0001
+    // 1   as 32bit:  0000 0000 0000 0000 0000 0000 0000 0001
+    // 0xF as 32bit:  0000 0000 0000 0000 0000 0000 0000 1111
+
+    // 0000 0000 0000 0000 0000 0000 0000 0000
+    // 0000 0000 0000 0000 0000 0000 0000 0001
+    // 3: 11  shr 1 -> 1
+    // 3: 101 shl 1 -> 110
+    print((0b1 shr 4).and(0xF))
+}
+
+
+private fun Int.positiveIntToBinary(): String {
+    if (this < 0) throw  NegativeNumberException()
+    val res = CharArray(CONSTANTS.INT_BINARY_SIZE) { '0' }
+    var num = this
+    var counter = 0
+    while (num != 0) {
+        res[(res.size - 1) - counter++] = (num % 2).digitToChar()
+        num /= 2
+    }
+    return String(res)
+}
