9593b7e279ae8f179e2d2d79b1772631a1d3982f
Orhan Rosenfeld <codeasus.orhan@outlook.com>
2023-04-18 12:41:25 +0400

    Configure the project for testing

---
+++ security/src/test/java/codeasus/projects/security/CryptographyFunctionalityTest.kt
@@ -0,0 +1,11 @@
+package codeasus.projects.security
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class CryptographyFunctionalityTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
